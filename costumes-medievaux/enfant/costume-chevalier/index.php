ec741e0ff8f31284f01e168e0d49c136b165659b
gmirmand <gmirmand@gmail.com>
2017-10-15 23:11:41 +0200

    evol

---
+++ costumes-medievaux/enfant/costume-chevalier/index.php
@@ -0,0 +1,288 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Costume médiéval de chevalier pour enfant</title>
+    <link rel="stylesheet" href="../../../src/css/main.css">
+    <link rel="stylesheet" type="text/css" href="../../../src/librairies/slick/slick.css"/>
+    <link rel="stylesheet" type="text/css" href="../../../src/librairies/slick/slick-theme.css"/>
+</head>
+<body>
+<div class="header" id="top">
+    <a href="/" class="logo">
+        <img src="../../../src/img/basics/logo.png" alt="Logo costume-medieval.com">
+    </a>
+    <div class="rs">
+        <a class="social-icon social-icon--github" href="https://github.com/gmirmand/costume-medieval">
+            <i class="fa fa-github"></i>
+        </a>
+        <a class="social-icon social-icon--twitter" href="https://twitter.com/">
+            <i class="fa fa-twitter"></i>
+        </a>
+        <a class="social-icon social-icon--facebook" href="https://www.facebook.com/">
+            <i class="fa fa-facebook"></i>
+        </a>
+    </div>
+    <div class="menu-left">
+        <a href="#">Armes</a>
+        <a href="#">Armures</a>
+        <a href="#">Bijoux</a>
+        <a href="#">Divers</a>
+    </div>
+    <div class="menu-right">
+        <a href="#">Nouveautés et idées cadeaux</a>
+        <a href="#">Achat</a>
+        <a href="#">Festival</a>
+    </div>
+</div>
+
+<div class="main">
+    <?php
+    $URL = $_SERVER['REQUEST_URI'];
+    $chemin = explode('/', $URL);
+    ?>
+    <div class="fil-ariane">
+        <a href="#">Accueil</a>
+        <?php
+        foreach ($chemin as $step) {
+            if ($step != '') {
+                $step = str_replace(".php", "", $step);
+                echo "<a href='#'>" . $step . "</a>";
+            }
+        }
+        ?>
+    </div>
+
+    <div class="top-content">
+        <div class="left">
+            <a href="#" class="type">Article</a>
+            <span class="date">2 Octobre 2017</span>
+            <h1>Costume enfant de chevalier</h1>
+            <p> Nous avons tous rêvé un jour d’incarner un chevalier, cela restera pour toujours notre rêve d’enfant.
+            </p>
+            <div class="header-meta-container">
+            <span class="header-meta">
+            <span>Par</span> <a href="#" title="Article par Yassine Benhabib" rel="author">Yassine Benhabib</a>
+            </span>
+                <span class="header-meta"> 216 vues</span>
+                <span>Aucun commentaire</span><br>
+            </div>
+            <div class="CTA">
+                <a href="#" class="button1">Inscription newsletter</a>
+            </div>
+        </div>
+        <div class="right">
+            <iframe width="560" height="315"
+                    src="https://www.youtube-nocookie.com/embed/aVS4W7GZSq0?rel=0&amp;controls=0&amp;showinfo=0"
+                    frameborder="0" allowfullscreen></iframe>
+        </div>
+        <div class="top-content-bg">
+        </div>
+    </div>
+    <div class="slider">
+        <div class="slider-for">
+            <div class="slide slider1"><img src="../../../src/img/article/cos1.png" alt="Costume enfant chevalier"/>
+            </div>
+            <div class="slide slider1"><img src="../../../src/img/article/cos2.png" alt="Costume enfant chevalier"/>
+            </div>
+            <div class="slide slider1"><img src="../../../src/img/article/cos3.png" alt="Costume enfant chevalier"/>
+            </div>
+            <div class="slide slider1"><img src="../../../src/img/article/cos4.png" alt="Costume enfant chevalier"/>
+            </div>
+            <div class="slide slider1"><img src="../../../src/img/article/cos5.png" alt="Costume enfant chevalier"/>
+            </div>
+            <div class="slide slider1"><img src="../../../src/img/article/cos5.png" alt="Costume enfant chevalier"/>
+            </div>
+        </div>
+        <div class="slider-nav">
+            <div class="slide slider1"><img src="../../../src/img/article/cos1.png" alt="Costume enfant chevalier"/>
+            </div>
+            <div class="slide slider1"><img src="../../../src/img/article/cos2.png" alt="Costume enfant chevalier"/>
+            </div>
+            <div class="slide slider1"><img src="../../../src/img/article/cos3.png" alt="Costume enfant chevalier"/>
+            </div>
+            <div class="slide slider1"><img src="../../../src/img/article/cos4.png" alt="Costume enfant chevalier"/>
+            </div>
+            <div class="slide slider1"><img src="../../../src/img/article/cos5.png" alt="Costume enfant chevalier"/>
+            </div>
+            <div class="slide slider1"><img src="../../../src/img/article/cos5.png" alt="Costume enfant chevalier"/>
+            </div>
+        </div>
+    </div>
+    <div class="content">
+        <p>Nobles Combattants à cheval, grands justiciers à l’épée, les chevaliers, appelés aussi cavaliers sont un
+            classique et un incontournable déguisement pour toute les fêtes de vos enfants. Que ce soit pour un
+            spectacle d’école où d’autre fêtes à thème médiéval ou renaissance, ce costume enfant chevalier sera parfait
+            pour votre petit en plus de couter très peu pour votre portefeuille.
+
+        <h2>Pour quelle occasion ?</h2>
+
+        <p>Ce costume enfant chevalier peut être enfiler pour toutes les fêtes d’anniversaires à thèmes ainsi que
+            d’autres fêtes médiévales comme “la fête du roi l’oiseau” au Puy-en-Velay ou encore pour les marchés
+            médiévaux tel que ‘’De Charlemagne aux chevaliers’’. Sa tenue de cavalier lui permettra de s’intégrer à
+            l’ambiance moyen âge de ces fêtes à merveille et ne manquera pas d’attirer l’attention des jolies princesses
+            aux alentours.
+
+        <h2>La qualité de nos déguisements</h2>
+
+        <p>Nos costumes d’enfants chevaliers sont confectionnés seulement avec de matériaux de très bonne qualitée par
+            de grands artisans travaillant le cuir avec une grande dextérité. Ces grands artisans sont reconnus au
+            Puy-en-Velay. Leurs créations sont mises en avant dans de grandes fêtes médiévales tel que le roi l’oiseau.
+            Solides et flexibles, vos enfants se sentiront très à l’aise dans leurs costumes d’enfant chevalier, le
+            tissu étant très doux et confortable. Vous n’avez rien à craindre en ce qui concerne la durabilité de ces
+            déguisements d’autre temps car ils sont choisis avec grand soin par nos artisans du Puy.
+
+        <h2>Quels sont les pièces qui composent le costume</h2>
+
+        <p>Ce costume d’enfant chevalier se compose d’un haut en tissu, d’un pantalon en coton, d’une cape en satin, de
+            couvre-bottes et ceinture en cuir. Juste de quoi avoir l’air d’un vrai petit chevalier de la table ronde !
+
+        <h2>Idée d’accessoire pour le costume médiéval</h2>
+
+        <p>Pour compléter à merveille ce costume d’enfant chevalier et d’avoir l’air d’un vrai guerrier, vous pouvez
+            vous offrir les accessoires suivants:
+        <ul>
+            <li>Casque en faux métal dorée</li>
+            <li>Armure en faux métal dorée</li>
+            <li>Épée en bois médiévale bleue</li>
+            <li>Bouclier en bois de l’ordre des chevaliers rouge</li>
+            <li>Fausse fourrure noire du roi l’oiseau</li>
+        </ul>
+
+        <p>Avec cela, vous pourrez ajouter une petite touche renaissance à votre accoutrement et épater toutes les
+            princesses à la taverne.
+
+
+        <h2>Tailles</h2>
+
+        <p>Pour les tailles de ce déguisement, nous recommandons les âges de 3 ans jusqu’à 11 ans.
+            Cependant pour accompagner votre enfant et de partager de bons moments avec lui, nous avons des versions
+            pour tout âges et sexes que ce soit pour des femmes, des hommes, des filles ou des garçons. Costumez vos
+            enfants pour la fête et vous aurez un vrai bataillon d’enfants chevaliers !
+
+    </div>
+    <div class="more">
+        <h2>D’autres produits similaires recommandés</h2>
+
+        <ul>
+            <li><h3><a href="#">robe médiévale noble</a></h3></li>
+            <li><h3><a href="#">costume seigneur medieval</a></h3></li>
+            <li><h3><a href="#">costume médiéval tavernière</a></h3></li>
+            <li><h3><a href="#">homme médiéval costume xxl</a></h3></li>
+            <li><h3><a href="#">costume médiéval archére</a></h3></li>
+        </ul>
+        <div class="social-icons">
+            <a class="social-icon social-icon--github" href="https://github.com/gmirmand/costume-medieval">
+                <i class="fa fa-github"></i>
+            </a>
+            <a class="social-icon social-icon--twitter" href="https://twitter.com/">
+                <i class="fa fa-twitter"></i>
+            </a>
+            <a class="social-icon social-icon--facebook" href="https://www.facebook.com/">
+                <i class="fa fa-facebook"></i>
+            </a>
+        </div>
+    </div>
+    <div class="comment">
+        <div class="item">
+            <div class="av">
+                <img src="../../../src/img/comment/av1.png" alt="Photo de profil michel homme">
+            </div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aliquid at dicta eligendi ex id impedit iure
+                laborum nihil, non, officiis optio pariatur porro possimus quaerat, quas veritatis voluptates
+                voluptatum!</p>
+        </div>
+        <div class="item">
+            <div class="av">
+                <img src="../../../src/img/comment/av2.png" alt="Photo de profil michelle femme">
+            </div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aliquid at dicta eligendi ex id impedit iure
+                laborum nihil, non</p>
+        </div>
+    </div>
+</div>
+<footer class="footer" role="contentinfo">
+    <div class="container">
+        <a href="https://www.roideloiseau.com/espace-presse/">
+            Espace presse </a>
+        <p>Retrouvez les dossiers de presse et les revues de presse de l'évènement.</p>
+        <a href="https://www.roideloiseau.com/contacts-professionnels/">
+            Contacts professionnels </a>
+        <p>Vous êtes un artiste, une compagnie de spectacle, un artisan, une entreprise, bienvenue dans nos fêtes.</p>
+    </div>
+
+    <div class="secondaryNav j-col-3">
+        <div class="secondaryNavInner">
+            <p class="secondaryNavTitle">Organisation</p>
+
+            <a target="_blank"
+               href="https://www.google.fr/maps/dir//29+Rue+Rapha%C3%ABl,+43000+Le+Puy-en-Velay/@45.0446238,3.8802456,17z/data=!4m16!1m7!3m6!1s0x47f5fa55105ce47b:0x6e630cb9f0d3b908!2s29+Rue+Rapha%C3%ABl,+43000+Le+Puy-en-Velay!3b1!8m2!3d45.0446238!4d3.8824343!4m7!1m0!1m5!1m1!1s0x47f5fa55105ce47b:0x6e630cb9f0d3b908!2m2!1d3.8824343!2d45.0446238">Association
+                du Roi de l'Oiseau</a>
+
+            <p itemprop="streetAddress">29, rue Raphaël</p>
+
+            <p><span itemprop="postalCode">43000</span> <span itemprop="addressLocality">Le Puy-en-Velay</span></p>
+        </div>
+
+        <div class="secondaryNavInner">
+            <p><a href="https://www.roideloiseau.com/euro-xvi/">EURO XVI</a></p>
+            <p><a href="https://www.roideloiseau.com/associations-participantes/">Associations participantes</a></p>
+        </div>
+
+        <div class="secondaryNavInner">
+            <p class="secondaryNavTitle">
+
+                Suivez nous sur
+            <div class="social-icons">
+                <a class="social-icon social-icon--twitter">
+                    <i class="fa fa-twitter"></i>
+                </a>
+                <a class="social-icon social-icon--instagram">
+                    <i class="fa fa-instagram"></i>
+                </a>
+                <a class="social-icon social-icon--facebook">
+                    <i class="fa fa-facebook"></i>
+                </a>
+            </div>
+        </div>
+    </div>
+
+    <div class="secondaryNav j-col-3">
+        <p class="secondaryNavTitle">Renseignements</p>
+
+
+        <a target="_blank" href="http://www.ot-lepuyenvelay.fr/" itemprop="name">Office de tourisme du Puy-en-Velay</a>
+        <div class="adresse" itemprop="address" itemscope="" itemtype="http://schema.org/PostalAddress">
+            <p itemprop="streetAddress">Place du Clauzel</p>
+
+            <p><span itemprop="postalCode">43000</span> <span itemprop="addressLocality">Le Puy-en-Velay</span></p>
+            <p class="phone" itemprop="telephone">Tél. 04 71 09 38 41</p>
+
+            <p class="mail"><a class="button" href="https://www.roideloiseau.com/formulaire-de-contact/">Nous
+                    contacter</a>
+            </p></div>
+
+        <a target="_blank" href="http://app.evalandgo.com/s/?id=JTk1byU5NW8lOUIlQUE=&amp;a=JTk1ayU5OGslOTklQTk="
+           class="button">Votre avis nous intéresse</a>
+
+        <a href="https://www.roideloiseau.com/nos-partenaires/" class="button partners_button">Nos partenaires</a>
+    </div>
+
+    <p class="copyright container clear">
+        <a href="https://www.roideloiseau.com/informations-legales/">Mentions légales</a> -
+        <a href="https://www.roideloiseau.com/plan-du-site/"> Plan du site</a>
+    </p>
+
+    <p>
+        <a href="#top" class="backToTop" id="backToTop" style="display: inline;">
+            <span>Retour en haut de la page</span>
+        </a>
+    </p>
+</footer>
+</body>
+
+<script src="../../../src/js/jquery.min.js"></script>
+<script src="../../../src/js/script.js"></script>
+<script type="text/javascript" src="../../../src/librairies/slick/slick.min.js"></script>
+
+</html>
